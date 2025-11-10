68c9763f980c66debad8b8e744f53b1494ab2b2c
Pablo <pablito@me.com>
2025-11-10 17:44:02 +0000

    chore: added new notebook for data processing that we did in data analytics topic 5 on 10th November.

--- data_source.py
+++ data_source.py
@@ -1,5 +1,34 @@
 import pandas as pd
+from enum import Enum
 
 def get_data_frame():
     df = pd.read_csv('data/customerChurnData.csv')
     return df
+
+class Dataset(Enum):
+    ENROL = "enrol"
+    PUPILS = "pupils"
+    SPEND = "spend"
+
+def get_bpp_5_data(dataset: Dataset, discard_rows = 3):
+    # Map enum values to file paths
+    file_map = {
+        Dataset.ENROL: "data/bpp_topic_5_data/API_SE.PRM.ENRL.TC.ZS_DS2_en_csv_v2_131293.csv",
+        Dataset.PUPILS: "data/API_SE.SEC.CUAT.UP.ZS_DS2_en_csv_v2_123269.csv",
+        Dataset.SPEND: "data/API_SE.PRM.ENRL_DS2_en_csv_v2_6679.csv",
+    }
+
+    # Get the correct file path based on the enum
+    file_path = file_map.get(dataset)
+    print(file_path)
+
+    if not file_path:
+        raise ValueError(f"Invalid dataset type: {dataset}")
+
+    # Read the corresponding CSV
+    df = pd.read_csv(file_path, skiprows=discard_rows)
+    return df
+
+# Example usage
+# data = get_bpp_5_data(Dataset.ENROL)
+# print(data.head())
--- helpers.py
+++ helpers.py
@@ -1,3 +1,6 @@
+import pandas as pd
+
+
 def is_significant(p_value, alpha=0.05):
     if p_value < alpha:
         print(f"✅ The difference is significant\nThe p-value: {p_value} is {alpha - p_value} less than the alpha: {alpha}\nThe null hypothesis is rejected")
@@ -8,3 +11,23 @@ def is_significant(p_value, alpha=0.05):
     else:
         print(f"❓The p-value: {p_value} is equal to the alpha: {alpha}\nI am not sure what this means in terms of rejecting the null hypothesis")
         return False
+
+def clean_bpp_5_dataframes(df: pd.DataFrame):
+    df.columns = df.columns.str.strip() # removes leading and trailing whitespace
+    df = df.dropna(axis=1, how="all") # drops any columns that ONLY contain NaN values
+    year_cols = [col for col in df.columns if col.isdigit()] # list comprehension for Years - contains only digits
+    df_melted = df.melt(
+        id_vars=["Country Name", "Country Code", "Indicator Name", "Indicator Code"],
+        value_vars=year_cols,
+        var_name="Year",
+        value_name="Value"
+    ) # Pandas melt method unpivots the df from wide to long format
+    # args:
+    # id_vars are the columns to leave as they are
+    # value_vars are the columns to unpivot into rows - the year cols
+    # var_name is the name for the new col in place of the year columns
+    # value_name is the name of the new col in place of the values that were in the year columns
+    df_melted["Year"] = pd.to_numeric(df_melted["Year"], errors="coerce") # the "Year" col values are converted to numeric format and any exceptions become NaN
+    df_melted = df_melted.dropna(subset=["Year", "Value"]) # drops any row where "Value" OR "Year" is NaN
+
+    return df_melted
